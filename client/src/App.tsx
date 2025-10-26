9f0edea7990cca08004b3df3ab716d3154c29cc0
Dariel Subiros <dsubiros@gmail.com>
2025-10-26 02:04:04 -0400

    Initial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+// import {createBrowserRouter} from "react-router-dom";
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <h1>home</h1> },
+  { path: '/about', element: <h1>about page</h1> },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
